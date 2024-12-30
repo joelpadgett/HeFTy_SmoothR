36bdf614fe5444eebde1493e408858cc2f271904
Tobias Stephan <73840881+tobiste@users.noreply.github.com>
2024-12-30 16:20:02 -0500

    cluster paths added

--- R/densify_paths.R
+++ R/densify_paths.R
@@ -2,12 +2,14 @@
 #'
 #' Adds extra points between the vertices of the path segments
 #'
-#' @param x t-T and GOF data of the modeled paths. Out put of [read_hefty_xlsx()].
-#' @param GOF_rank numeric. Selects only the n highest GOF ranked paths.
-#' @param n integer. Adds `n` equally-spaced extra points along each path
-#' segment (between vertices). The default is 10.
+#' @param x t-T and GOF data of the modeled paths. Output of [read_hefty_xlsx()].
+#' @param GOF_rank numeric. Selects only the `GOF_rank`-th highest GOF ranked paths.
+#' @param ... arguments passed to [smoothr::densify()]. `n` (integer) adds `n` (10 by
+#' default) equally-spaced extra points along each path
+#' segment (between vertices). Or you specify `max_distance` (numeric) to add
+#' points at a maximum distance of `max_distance` (in Myr) from each other.
 #' @param samples integer. Number of random samples of the data. This number should
-#' be less or equal then the`nrow(x)`. Be aware that a large number will require
+#' be less or equal then the `nrow(x)`. Be aware that a large number will require
 #' a long(!) processing time. The default is `100`.
 #' @param replace logical. Should sampling be with replacement?
 #'
@@ -21,15 +23,14 @@
 #'
 #' @examples
 #' data(s14MM_v1)
-#' densify_paths(s14MM_v1)
-densify_paths <- function(x, GOF_rank = 10L, n = 10L, samples = 100L, replace = FALSE) {
+#' densify_paths(s14MM_v1, n = 10)
+#' densify_paths(s14MM_v1, n = 10, max_distance = 1)
+densify_paths <- function(x, GOF_rank = 10L, ..., samples = 100L, replace = FALSE) {
   L1 <- L2 <- X <- Y <- numeric()
   segment <- time <- temperature <- NULL
 
   # Subset Data by Highest N GOF Values (If Desired)
-
   x$rank <- dplyr::dense_rank(-x$Comp_GOF) # Rank Comp_GOF values with lowest rank (1) being highest GOF value
-
   hs.input <- dplyr::filter(x, rank <= GOF_rank) # Remove all t-T points with rank greater than N; new table shows top N GOF t-T points
 
   # Subset data by random N number of segments (If Desired)
@@ -51,21 +52,25 @@ densify_paths <- function(x, GOF_rank = 10L, n = 10L, samples = 100L, replace = 
     replace = replace
   )
 
-
-
   res <- hs.input %>% # filter the original data for the segments selected in unique_segments
     dplyr::filter(segment %in% subset_segments) %>% # include only rows that match the selected segments
     dplyr::mutate(x = time, y = temperature) %>%
     sf::st_as_sf(coords = c("x", "y")) %>% # make a spatial feature where x and y are the spatial coordinates
+    dplyr::group_by(segment) %>%
     dplyr::summarise(do_union = FALSE) %>%
     sf::st_cast("LINESTRING") %>%
-    sf::st_cast("MULTILINESTRING") %>%
-    smoothr::densify(n = n) # this sets the number of points that will be added to each segment. This can be changed as desired
+    # sf::st_cast("MULTILINESTRING") %>%
+    smoothr::densify(...) # this sets the number of points that will be added to each segment. This can be changed as desired
+
 
   res_coords <- sf::st_coordinates(res)
+  lookup <- data.frame(L1 = unique(res_coords[, 3]), segment = dplyr::filter(hs.input, segment %in% subset_segments) |> dplyr::pull(segment) |> unique())
 
-  dplyr::bind_cols(res, res_coords) %>%
-    sf::st_drop_geometry() %>%
+  res_coords |>
+    dplyr::as_tibble() |>
+    dplyr::left_join(lookup, dplyr::join_by(L1)) |>
+    # dplyr::bind_cols(res, res_coords) %>%
+    # sf::st_drop_geometry() %>%
     dplyr::rename(time = X, temperature = Y) %>%
     dplyr::select(-L1, -L2)
 }
---
+++ R/path_cluster.R
@@ -0,0 +1,37 @@
+#' Cluster t-T paths
+#'
+#' Hierarchical clustering of t-T paths based on the Hausdorff distance
+#' between paths.
+#'
+#' @param x t-T and GOF data of the modeled paths. Output of [read_hefty_xlsx()].
+#' @param ncluster an integer scalar or vector with the desired number of groups
+#' @param dist character; algorithm to calculate a dissimilarity matrix
+#' (distance) for lines; one of `Euclidean`, `Hausdorff` (the default) or
+#' `Frechet`.
+#'
+#' @return a tibble with the path `segment` (integer) and `cluster` (factor)
+#' @export
+#'
+#' @importFrom sf st_as_sf st_distance
+#' @importFrom dplyr summarise group_by tibble
+#' @importFrom stats hclust cutree as.dist
+#'
+#' @examples
+#' \dontrun{
+#' data(s14MM_v1)
+#' cluster_paths(s14MM_v1, ncluster = 3)
+#' }
+cluster_paths <- function(x, ncluster, dist = c("Hausdorff", "Frechet", "Euclidean")) {
+  x_lines <- x |>
+    sf::st_as_sf(coords = c("time", "temperature")) |>
+    dplyr::group_by(segment) |>
+    dplyr::summarise(do_union = FALSE) |>
+    sf::st_cast("LINESTRING")
+
+  dist <- match.arg(dist)
+  dmat <- sf::st_distance(x_lines, which = dist)
+
+  cl <- stats::hclust(stats::as.dist(dmat))
+  dplyr::tibble(segment = x_lines$segment, cluster = as.factor(stats::cutree(cl, ncluster)))
+}
+
